6de18fe6f325c0225f4f2044a65a02dac7d7e281
Adam Jones <machieajones@gmail.com>
2020-04-21 14:51:22 +0100

    Updates

---
+++ precache-manifest.bdf1d46d86ed1f01dd846a59dc624c12.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20da724e07b18359fd345954ea037627",
+    "url": "/spotify-ui-reactjs/index.html"
+  },
+  {
+    "revision": "ff4c1a8948355f099ca3",
+    "url": "/spotify-ui-reactjs/static/css/main.d826acca.chunk.css"
+  },
+  {
+    "revision": "335e742f7cb07e753266",
+    "url": "/spotify-ui-reactjs/static/js/2.b678c49e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spotify-ui-reactjs/static/js/2.b678c49e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4c1a8948355f099ca3",
+    "url": "/spotify-ui-reactjs/static/js/main.6ca5b412.chunk.js"
+  },
+  {
+    "revision": "56d00f48061a724b7953",
+    "url": "/spotify-ui-reactjs/static/js/runtime-main.86bb7061.js"
+  },
+  {
+    "revision": "6baed2bf580964bec9559ad83caee43d",
+    "url": "/spotify-ui-reactjs/static/media/CircularStd-Bold.6baed2bf.otf"
+  },
+  {
+    "revision": "6365c40aa59d462f1cc52ccce9635cb4",
+    "url": "/spotify-ui-reactjs/static/media/CircularStd-Book.6365c40a.otf"
+  },
+  {
+    "revision": "3005871a5a18de80182ce2cf5a7167bd",
+    "url": "/spotify-ui-reactjs/static/media/home.3005871a.svg"
+  },
+  {
+    "revision": "d464e65a9d45f9b6b3056b0019f63a1b",
+    "url": "/spotify-ui-reactjs/static/media/library.d464e65a.svg"
+  },
+  {
+    "revision": "78b8b9853183e29bd82d98aafc60340c",
+    "url": "/spotify-ui-reactjs/static/media/play.78b8b985.svg"
+  },
+  {
+    "revision": "0f6375428d8f094acc24d7e67c4ebcc1",
+    "url": "/spotify-ui-reactjs/static/media/search.0f637542.svg"
+  }
+]);
